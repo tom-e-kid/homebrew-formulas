73c7a2b130c1c6434a6ffd3e95472bf7356721c3
Tomohiko Ikeda <tom_e_kid@mac.com>
2018-03-14 16:56:28 +0900

    add gitexport.

---
+++ gitexport.rb
@@ -0,0 +1,9 @@
+ class Gitexport < Formula
+  desc "Create disk image from git archive"
+  homepage ""
+  url "https://github.com/tom-e-kid/gitexport/releases/download/v1.0.0/gitexport"
+  sha256 "4e25fa3e35278c0164388ee6bb8bfdf2ffc16e5b16c8adf84cc81fbcfccb86ea"
+   def install
+	bin.install
+   end
+end
