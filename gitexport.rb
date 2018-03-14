ef71cda6d3be0086a4b3cda8239a48ab494b300d
Tomohiko Ikeda <tom_e_kid@mac.com>
2018-03-14 17:43:58 +0900

    Update bin.install

--- gitexport.rb
+++ gitexport.rb
@@ -1,9 +1,10 @@
- class Gitexport < Formula
+class Gitexport < Formula
   desc "Create disk image from git archive"
   homepage ""
-  url "https://github.com/tom-e-kid/gitexport/releases/download/v1.0.0/gitexport"
+  url "https://github.com/tom-e-kid/gitexport/releases/download/v1.0.1/gitexport.sh"
   sha256 "4e25fa3e35278c0164388ee6bb8bfdf2ffc16e5b16c8adf84cc81fbcfccb86ea"
-   def install
-	    bin.install "gitexport"
-   end
+
+  def install
+    bin.install "gitexport.sh" => "gitexport"
+  end
 end
