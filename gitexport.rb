e9b2314e0ecaf67a59b3365be8e2c0b2b750f18b
Tomohiko Ikeda <tom_e_kid@mac.com>
2018-03-14 17:18:11 +0900

    Fix bin.install

--- gitexport.rb
+++ gitexport.rb
@@ -4,6 +4,6 @@
   url "https://github.com/tom-e-kid/gitexport/releases/download/v1.0.0/gitexport"
   sha256 "4e25fa3e35278c0164388ee6bb8bfdf2ffc16e5b16c8adf84cc81fbcfccb86ea"
    def install
-	bin.install
+	    bin.install "gitexport"
    end
 end
